d80347b3bd9153d19cb0157666f1020d7e4a68ba
piachimov <Petru.Iachimov@endava.com>
2018-10-08 12:27:39 +0300

    'Check'

--- app/src/main/java/apple/example/com/amintershiptask1/adapter/CallListAdapter.java
+++ app/src/main/java/apple/example/com/amintershiptask1/adapter/CallListAdapter.java
@@ -1,11 +1,15 @@
 package apple.example.com.amintershiptask1.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -13,6 +17,7 @@ import android.widget.TextView;
 import java.util.List;
 
 import apple.example.com.amintershiptask1.R;
+import apple.example.com.amintershiptask1.content.CallLogContent;
 import apple.example.com.amintershiptask1.models.ContactInformation;
 
 public class CallListAdapter extends RecyclerView.Adapter<CallListAdapter.PersonsViewHolder>{
@@ -49,6 +54,7 @@ public class CallListAdapter extends RecyclerView.Adapter<CallListAdapter.Person
     public PersonsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.source_layout, parent, false);
         return new PersonsViewHolder(itemView);
+
     }
 
     @Override
@@ -68,7 +74,11 @@ public class CallListAdapter extends RecyclerView.Adapter<CallListAdapter.Person
                 holder.textType.setTextColor(Color.parseColor("#FFF74044"));
             } break;
         }
-
+            if (person.getName() == null){
+            holder.textName.setText("Unknown number");
+            } else  {
+            holder.textName.setText(person.getName());
+            }
             holder.textNumber.setText(person.getNumber());
 
             int timeInSeconds = Integer.parseInt(person.getDuration());
@@ -83,6 +93,47 @@ public class CallListAdapter extends RecyclerView.Adapter<CallListAdapter.Person
                 sec = "0" + sec;
             }
             holder.textDuration.setText(min + ":" + sec);
+
+            holder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final Dialog editDialog = new Dialog(context);
+                    editDialog.setContentView(R.layout.edit_dialog);
+                    Button editItem = editDialog.findViewById(R.id.editItem);
+                    Button deleteItem = editDialog.findViewById(R.id.deleteItem);
+                    final EditText editName = editDialog.findViewById(R.id.editName);
+                    final EditText editNumber = editDialog.findViewById(R.id.editNumber);
+                    final EditText editDuration = editDialog.findViewById(R.id.editDuration);
+                    final EditText editType = editDialog.findViewById(R.id.editType);
+                    editName.setText(person.getName());
+                    editNumber.setText(person.getNumber());
+                    editDuration.setText(person.getDuration());
+                    editType.setText(person.getType());
+                    editItem.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            CallLogContent callLogContent = new CallLogContent(context);
+                            callLogContent.editCall(new ContactInformation(person.getId(), editName.getText().toString(), editDuration.getText().toString(), editType.getText().toString(), editNumber.getText().toString()));
+                            personsList = callLogContent.getCallLogs();
+                            notifyDataSetChanged();
+                            editDialog.hide();
+                            editDialog.dismiss();
+                        }
+                    });
+                    deleteItem.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            CallLogContent callLogContent = new CallLogContent(context);
+                            callLogContent.deleteCall(person.getId());
+                            personsList = callLogContent.getCallLogs();
+                            notifyDataSetChanged();
+                            editDialog.hide();
+                            editDialog.dismiss();
+                        }
+                    });
+                    editDialog.show();
+                }
+            });
         }
 
     @Override
--- app/src/main/java/apple/example/com/amintershiptask1/content/CallLogContent.java
+++ app/src/main/java/apple/example/com/amintershiptask1/content/CallLogContent.java
@@ -1,10 +1,11 @@
 package apple.example.com.amintershiptask1.content;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CallLog;
-
 
 
 import java.util.ArrayList;
@@ -17,24 +18,43 @@ public class CallLogContent {
 
     private Context context;
 
-    public CallLogContent(Context context){
+    public CallLogContent(Context context) {
         this.context = context;
     }
 
-        public List<ContactInformation> getCallLogs(){
+    public List<ContactInformation> getCallLogs() {
         List<ContactInformation> contacts = new ArrayList<>();
         int id = 0;
         Uri allCalls = Uri.parse("content://call_log/calls");
         Cursor cursor = context.getContentResolver().query(allCalls, null, null, null, null);
-        while (cursor.moveToNext()){
+        while (cursor.moveToNext()) {
             String name = cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NAME));
             String duration = cursor.getString(cursor.getColumnIndex(CallLog.Calls.DURATION));
             String type = cursor.getString(cursor.getColumnIndex(CallLog.Calls.TYPE));
             String number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
-            contacts.add(new ContactInformation(id,name,duration,type, number));
+            contacts.add(new ContactInformation(id, name, duration, type, number));
             id++;
         }
         return contacts;
+    }
 
+    public void editCall(ContactInformation person) {
+        Uri allCalls = Uri.parse("content://call_log/calls");
+        ContentResolver contentResolver = context.getContentResolver();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(CallLog.Calls.NUMBER, person.getNumber());
+        contentValues.put(CallLog.Calls.CACHED_NAME, person.getName());
+        contentValues.put(CallLog.Calls.DURATION, person.getDuration());
+        contentValues.put(CallLog.Calls.TYPE, person.getType());
+        String selectionClause = CallLog.Calls._ID + " = ?";
+        String[] selectionArgs = {person.getId() + ""};
+        contentResolver.update(allCalls, contentValues, selectionClause, selectionArgs);
+    }
+
+    public void deleteCall(int id) {
+        Uri allCalls = Uri.parse("content://call_log/calls");
+        String selectionClause = CallLog.Calls._ID + " = ?";
+        String[] selectionArgs = {id + ""};
+        context.getContentResolver().delete(allCalls, selectionClause, selectionArgs);
     }
 }
